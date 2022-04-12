79846f1029bde35e9d39bff80e31229df8cc183a
roilin <fabien705cccc@gmail.com>
2022-04-12 21:33:17 +0800

    init spring boot project

---
+++ src/test/java/com/roi/springbootmall/SpringBootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roi.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
